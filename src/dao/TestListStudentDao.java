960fd240886818a0e25b2aabe9ad8783836aae42
s-shun0 <tky2402129@stu.o-hara.ac.jp>
2025-05-07 11:37:18 +0900

    Add files via upload

--- src/dao/TestListStudentDao.java
+++ src/dao/TestListStudentDao.java
@@ -1,5 +1,81 @@
 package dao;
 
-public class TestListStudentDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import bean.School;
+import bean.Student;
+import bean.Subject;
+import bean.TestListStudent;
+
+public class TestListStudentDao extends Dao{
+	private String baseSql="select * from test";
+
+	public List<TestListStudent> postFilter(ResultSet rSet){
+		List<TestListStudent> list = new ArrayList<>();
+		SchoolDao sDao = new SchoolDao();
+		SubjectDao subDao = new SubjectDao();
+		try{
+			while (rSet.next()){
+				School school = sDao.get(rSet.getString("test.school_cd"));
+
+				Subject subject = subDao.get(rSet.getString("test.subject_cd"),school);
+
+
+				TestListStudent student = new TestListStudent();
+				student.setSubjectName(subject.getName());
+				student.setSubjectCd(subject.getCd());
+				student.setNum(rSet.getInt("test.no"));
+				student.setPoint(rSet.getInt("test.point"));
+				list.add(student);
+			}
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		return list;
+
+	}
+
+	public List<TestListStudent> filter(Student student)throws Exception{
+		List<TestListStudent> list = new ArrayList<>();
+		Connection connection = getConnection();
+		PreparedStatement statement= null;
+		ResultSet rSet=null;
+		//join
+		String join = " join student on test.student_no = student.no ";
+
+		String condition = " where student.no=? and test.class_num=? and test.school_cd=?";
+		String order = " order by test.student_no asc, test.no asc";
+		try{
+			statement=connection.prepareStatement(baseSql+join+condition+order);
+			statement.setString(1, student.getNo());
+			statement.setString(2, student.getClassNum());
+			statement.setString(3, student.getSchool().getCd());
+			rSet=statement.executeQuery();
+
+			list=postFilter(rSet);
+		}catch (Exception e){
+			throw e;
+		}finally {
+			if (statement != null){
+				try {
+					statement.close();
+				}catch(SQLException sqle) {
+					throw sqle;
+				}
+			}
+			if (connection != null){
+				try {
+					connection.close();
+				} catch(SQLException sqle) {
+					throw sqle;
+				}
+			}
+		}
+		return list;
+	}
 }
--- src/dao/TestListSubjectDao.java
+++ src/dao/TestListSubjectDao.java
@@ -1,5 +1,82 @@
 package dao;
 
-public class TestListSubjectDao {
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.School;
+import bean.Subject;
+import bean.TestListSubject;
+
+public class TestListSubjectDao extends Dao{
+	private String baseSql = "select * from test";
+
+	public List<TestListSubject> postFilter(ResultSet rSet){
+		List<TestListSubject> list = new ArrayList<>();
+
+		try{
+			while (rSet.next()){
+				TestListSubject subject = new TestListSubject();
+
+				subject.setEntYear(rSet.getInt("student.ent_year"));
+				subject.setStudentNo(rSet.getString("test.student_no"));
+				subject.setStudentName(rSet.getString("student.name"));
+				subject.setClassNum(rSet.getString("student.class_num"));
+				subject.putPoint(rSet.getInt("test.no"),rSet.getInt("test.point"));
+				list.add(subject);
+			}
+		}catch(Exception  e){
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public List<TestListSubject> filter(int entYear,String classNum,Subject subject,School school)throws Exception{
+		List<TestListSubject> list = new ArrayList<>();
+		Connection connection = getConnection();
+		PreparedStatement statement = null;
+		ResultSet rSet = null;
+
+		// SQL文のテーブル連結
+		String join = " join student on test.student_no = student.no";
+		// SQL文の条件
+		String condition = " where test.subject_cd=? and test.school_cd=? and student.ent_year=?";
+		// SQL文のソート
+		String order = " order by test.student_no asc, test.no asc";
+
+
+
+
+		try{
+			statement = connection.prepareStatement(baseSql + join + condition + order);
+			statement.setString(1,subject.getCd());
+			statement.setString(2, school.getCd());
+			statement.setInt(3, entYear);
+			rSet=statement.executeQuery();
+
+			list = postFilter(rSet);
+		}catch (Exception e){
+			throw e;
+		} finally {
+			if (statement != null) {
+				try{
+					statement.close();
+				} catch (SQLException sqle){
+					throw sqle;
+				}
+			}
+			if (connection != null){
+				try{
+					connection.close();
+				} catch (SQLException sqle){
+					throw sqle;
+				}
+			}
+		}
+		return list;
+	}
 }
