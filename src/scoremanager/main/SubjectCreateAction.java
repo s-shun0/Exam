3d2638695a2396bc119ee9538644f36451e75685
Al-lin07 <tky2402134@stu.o-hara.ac.jp>
2025-05-21 11:34:31 +0900

    プロジェクト新規作成

--- src/scoremanager/main/SubjectCreateAction.java
+++ src/scoremanager/main/SubjectCreateAction.java
@@ -3,12 +3,20 @@ package scoremanager.main;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.School;
+import bean.Subject;
+import bean.Teacher;
 import tool.Action;
 
 public class SubjectCreateAction extends Action {
-
     @Override
-    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        request.getRequestDispatcher("subject_create.jsp").forward(request, response);
+    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        Teacher teacher = (Teacher) req.getSession().getAttribute("user");
+        School school = teacher.getSchool();
+
+        req.setAttribute("subject", new Subject());
+        req.setAttribute("errors", new java.util.HashMap<String, String>());
+
+        req.getRequestDispatcher("subject_create.jsp").forward(req, res);
     }
 }
