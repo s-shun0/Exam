e413ea8c985d95ecd140a5fc905c418161a89f19
Al-lin07 <tky2402134@stu.o-hara.ac.jp>
2025-05-21 11:37:45 +0900

    プロジェクト新規作成

--- src/scoremanager/main/SubjectUpdateAction.java
+++ src/scoremanager/main/SubjectUpdateAction.java
@@ -4,18 +4,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import bean.Subject;
+import bean.Teacher;
 import dao.SubjectDao;
 import tool.Action;
 
 public class SubjectUpdateAction extends Action {
-    @Override
-    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        String cd = request.getParameter("cd");
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		// 取得參數
+		String cd = req.getParameter("cd");
 
-        SubjectDao sDao = new SubjectDao();
-        Subject subject = sDao.get(cd);
+		// 取得登入中使用者
+		Teacher teacher = (Teacher) req.getSession().getAttribute("user");
 
-        request.setAttribute("subject", subject);
-        request.getRequestDispatcher("subject_update.jsp").forward(request, response);
-    }
+		// 取得該科目資料
+		SubjectDao dao = new SubjectDao();
+		Subject subject = dao.get(cd, teacher.getSchool());
+
+		// 設定至畫面
+		req.setAttribute("subject", subject);
+
+		// 前往更新畫面
+		req.getRequestDispatcher("subject_update.jsp").forward(req, res);
+	}
 }
